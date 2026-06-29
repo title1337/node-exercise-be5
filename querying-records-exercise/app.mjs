85ec55ebca71b086a665e925efb09f2a2ceb6918
ttestmaill01@gmail.com <ttestmaill01@gmail.com>
2026-06-30 06:57:50 +0700

    Fix EXCreate a pool exercise

--- querying-records-exercise/app.mjs
+++ querying-records-exercise/app.mjs
@@ -1,21 +1,43 @@
-import express from "express";
-import { pool } from "./db.mjs";
+import express from 'express';
+import { pool } from './db.mjs';
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 
-app.get("/movies", async (req, res) => {
-	const result = await pool.query("select * from movies");
+app.get('/movie', async (req, res) => {
+  try {
+    const result = await pool.query('select * from movies');
+    return res.json({
+      data: result.rows,
+    });
+  } catch (error) {
+    return res.json({
+      message: error.message,
+    });
+  }
+});
 
-	return res.json({
-		data: result.rows,
-	});
+app.get('/movie/:movieID', async (req, res) => {
+  try {
+    const movieID = req.params.movieID;
+    const result = await pool.query(
+      'select * from movies where movie_id = $1',
+      [movieId],
+    );
+    return res.json({
+      data: result.rows[0],
+    });
+  } catch (error) {
+    return res.json({
+      message: 'ไม่สามารถเชื่อมต่อ Database ได้',
+    });
+  }
 });
 
 // 📍 **** สร้าง API เพื่อใช้ในการดูข้อมูลหนังแต่ละเรื่องด้วย movieId ตรงนี้ ****
 
 app.listen(port, () => {
-	console.log(`🚀 Server is running at ${port}`);
+  console.log(`🚀 Server is running at ${port}`);
 });
